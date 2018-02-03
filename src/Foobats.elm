9451a32280fc37e6f03a63122f57ad8b150c0195
Murphy Randle <murphyrandle@gmail.com>
2018-02-03 11:15:27 -0700

    Clean up the text a bit

--- src/Foobats.elm
+++
@@ -1,5 +0,0 @@
-module Foobats exposing (fluff)
-
-
-fluff =
-    "helloooooooooo hahah wow!"
--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,14 @@
 module Main exposing (..)
 
-import Html 
+import Html
 import Platform exposing (Program)
-import Foobats
 
 
 main : Program {} () msg
-main = Html.programWithFlags
-    { view = \a -> Html.text Foobats.fluff
-    , init = \_ -> () ! []
-    ,subscriptions  = \model -> Sub.none
-    , update = \msg model -> model ! []
-    }
-
+main =
+    Html.programWithFlags
+        { view = \a -> Html.text "Welcome to Elm! \x1F916💥"
+        , init = \_ -> () ! []
+        , subscriptions = \model -> Sub.none
+        , update = \msg model -> model ! []
+        }
